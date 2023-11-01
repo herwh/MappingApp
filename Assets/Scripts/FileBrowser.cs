007d65a97c19c60e2b335bd4f31ab4f6d9c03219
herwh <herwh@yandex.ru>
2023-11-01 18:46:43 +0300

    Added pin data input to edit menu

--- Assets/Scripts/FileBrowser.cs
+++ Assets/Scripts/FileBrowser.cs
@@ -1,6 +1,68 @@
+using System.Collections;
+using System.IO;
+using SFB;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class FileBrowser : MonoBehaviour
 {
-    
+    [SerializeField]private RawImage _image;
+    public void InputImageButtonOnClick()
+    {
+        string[] paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "png", false);
+        
+        if (paths.Length > 0)
+        {
+            StartCoroutine(OutputRoutineOpen(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+
+    private void Start()
+    {
+        var dirPath = Application.dataPath + "/SaveImages/";
+        var fullPath = dirPath + "Image" + ".png"; 
+        
+        if(!File.Exists(fullPath)) 
+        {
+            return;
+        }
+        
+        var texture = new Texture2D(3, 3);
+        texture.LoadImage(File.ReadAllBytes(fullPath));
+        
+        _image.texture = texture;
+    }
+
+    private IEnumerator OutputRoutineOpen(string url)
+    {
+       UnityWebRequest www = UnityWebRequest.Get(url);
+
+       yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+        else
+        {
+            var bytes = www.downloadHandler.data;
+            
+            var texture = new Texture2D(3, 3);
+            texture.LoadImage(bytes);
+            _image.texture = texture;
+            
+            var dirPath = Application.dataPath + "/SaveImages/";
+            var fullPath = dirPath + "Image" + ".png"; 
+            
+            if(!Directory.Exists(dirPath)) 
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            File.WriteAllBytes(fullPath, bytes);
+            
+            UnityEditor.AssetDatabase.Refresh();
+        }
+    }
 }
